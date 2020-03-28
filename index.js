f5212864e84f617e876df80c1ad6e7a7e9dd5ee3
Micheal Ojo <yomiojo001@gmail.com>
2020-03-28 14:51:45 +0100

    button-interactivity

--- index.js
+++ index.js
@@ -18,3 +18,62 @@ function soundTimer(){
         setTimeout(playSound, 1200 * i)
     }
 }
+
+function updateValue(key, value){
+    if(value <0){
+        value = 0;
+        console.log("Positive Numers Only");
+    }
+
+    if(key == "seconds"){
+        if(value <10){
+            value = "0" + value;
+        }
+
+        if(value>59){
+            value = 59;
+        }
+    }
+    $("#" + key).html(value || 0);
+    timerValue[key] = value;
+
+    console.log(timerValue.minutes);
+    console.log(timerValue.seconds);
+    
+}
+
+(function detectChanges(key){
+    console.log("Detect Changes");
+
+    let input = "#" + key + "-input";
+
+    $(input).change(function() {
+        updateValue(key, $(input).val());
+    });
+
+    return arguments.callee;
+})("minutes")("seconds");
+
+function startTimer(){
+    buttonManager(["start", false], ["pause", true], ["stop", true])
+}
+
+function pauseTimer(){
+    buttonManager(["start", true], ["pause", false], ["stop", true])
+}
+
+function stopTimer(){
+    buttonManager(["start", true], ["pause", false], ["stop", false])
+}
+
+
+function buttonManager(...buttonsArray){
+    for(let i = 0; i < buttonsArray.length; i++){
+        let button = "#" + buttonsArray[i][0] + "-button";
+        if(buttonsArray[i][1]){
+            $(button).removeAttr("disabled");
+        }else{
+            $(button).attr("disabled", "disabled");
+        }
+    }
+}
